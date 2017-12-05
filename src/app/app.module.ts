e257f52f8b69eb80c6bc9d0b767ee464a3407260
Kenny Lai <kenny.lai@ctt.co.uk>
2017-12-05 14:15:08 +0000

    Refactored the code so its in a clean state for other branches to be created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { listOfReducers } from './state-management/ngrx-index';
 import { CounterFacade } from './state-management/facade/counter-facade';
-import { FormsModule } from '@angular/forms';
+
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -18,10 +18,10 @@ import { FooterComponent } from './footer/footer.component';
     FooterComponent,
   ],
   imports: [
-    MDBBootstrapModule.forRoot(),
     BrowserModule,
-    StoreModule.forRoot(listOfReducers),
     FormsModule,
+    StoreModule.forRoot(listOfReducers),
+    MDBBootstrapModule.forRoot(),
   ],
   exports: [
   ],
