4f23275537695d609da7144edfd1224496b96fe7
Kenny Lai <kenny.lai@ctt.co.uk>
2017-12-05 15:11:10 +0000

    Added basic routing and added a home page with bouncing animations for the lulz£

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,18 +10,44 @@ import { CounterFacade } from './state-management/facade/counter-facade';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PkInputSearchComponent } from './components/pk-input-search/pk-input-search.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'galleries',
+    component: PkInputSearchComponent,
+    data: { title: 'Heroes List' }
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  { path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PkInputSearchComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    PkInputSearchComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     StoreModule.forRoot(listOfReducers),
     MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    )
   ],
   exports: [
   ],
