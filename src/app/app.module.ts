cca33ab767066c8ff9ae1e7942caffabf4c4eb9c
Kenny Lai <kenny.lai@ctt.co.uk>
2017-12-05 11:05:51 +0000

    Included the MDBbootstrap module in the app module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,15 @@ import { AppComponent } from './app.component';
 import { listOfReducers } from './state-management/ngrx-index';
 import { CounterFacade } from './state-management/facade/counter-facade';
 import { FormsModule } from '@angular/forms';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { NO_ERRORS_SCHEMA } from '@angular/compiler/src/core';
 
 @NgModule({
   declarations: [
     AppComponent,
   ],
   imports: [
+    MDBBootstrapModule.forRoot(),
     BrowserModule,
     StoreModule.forRoot(listOfReducers),
     FormsModule,
@@ -22,6 +25,7 @@ import { FormsModule } from '@angular/forms';
   providers: [
     CounterFacade
   ],
+  schemas: [ NO_ERRORS_SCHEMA ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
