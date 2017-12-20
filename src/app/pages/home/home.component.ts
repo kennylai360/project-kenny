e0928206ba32a82250debc5435a5b7bd7458978a
Kenny Lai <kenny.lai@ctt.co.uk>
2017-12-20 14:30:12 +0000

    Removed unused counter facade variables and unused imports

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { CounterFacade } from '../../state-management/counter/counter-facade';
 
 @Component({
   selector: 'app-home',
@@ -9,13 +8,9 @@ import { CounterFacade } from '../../state-management/counter/counter-facade';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private counterFacade:  CounterFacade) { }
+  constructor() { }
 
   ngOnInit() {
-  }
-
-  public addOne() {
-    this.counterFacade.incrementCounter();
   }
 
 }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,8 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { GalleryCoverModule } from '../../components/gallery-cover/gallery-cover.module';
-import { NgxPaginationModule } from 'ngx-pagination';
-import { GalleriesComponent } from '../galleries/galleries.component';
 import { HomeComponent } from './home.component';
 import { RouterModule } from '@angular/router';
 
