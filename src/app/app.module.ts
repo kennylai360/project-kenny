3e494ddf69f23e94cce8843fc0f1ba1974b84556
Kenny Lai <kenny.lai@ctt.co.uk>
2019-01-23 14:33:50 +0000

    PROK-777 updated it so we are using the meta reducer to help us spit out the NGRX state action, similar to the loggers without needing us to install any third-party library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,7 +26,19 @@ import { GalleryAlbumComponent } from './pages/galleries/gallery-album/gallery-a
 import { AppFacade } from './state-management/app/app.facade';
 import { NgxPaginationModule } from 'ngx-pagination';
 
-export const metaReducers = environment.production ? [] : [];
+export function metaReducerLogger(reducer: ActionReducer<any>): ActionReducer<any> {
+  return (state: any, action: any) => {
+    console.groupCollapsed(action.type);
+    const nextState = reducer(state, action);
+    console.log(`%c prev state`, `color: #9E9E9E; font-weight: bold`, state);
+    console.log(`%c action`, `color: #03A9F4; font-weight: bold`, action);
+    console.log(`%c next state`, `color: #4CAF50; font-weight: bold`, nextState);
+    console.groupEnd();
+    return nextState;
+  };
+}
+
+export const metaReducers = environment.production ? [] : [metaReducerLogger];
 
 const appRoutes: Routes = [
   {
