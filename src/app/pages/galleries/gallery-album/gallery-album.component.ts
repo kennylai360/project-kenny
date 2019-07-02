ed75ba224f1d7b4bd1f5202816e4a9ccc4bd04bb
Kenny Lai <kenny360@gmail.com>
2019-07-02 22:15:17 +0100

    PROK-2 updated the modal so that it will not open until the image has fully loaded

--- src/app/pages/galleries/gallery-album/gallery-album.component.ts
+++ src/app/pages/galleries/gallery-album/gallery-album.component.ts
@@ -19,6 +19,8 @@ export class GalleryAlbumComponent implements OnInit, OnDestroy {
 
   private routeSubscription: Subscription;
 
+  private loadedImagesUrl: Array<string> = [];
+
   public albumData: Observable<IGalleryCover> = of({
     albumId: '',
     imgUrl: ''
@@ -46,8 +48,19 @@ export class GalleryAlbumComponent implements OnInit, OnDestroy {
   }
 
   public openModal(imageId: number, imageUrl: string, horizontal: boolean) {
-    this.appFacade.updateSelectedImage(imageUrl, imageId, horizontal);
-    this.appFacade.openModal();
+    this.loadedImagesUrl.map((loadedImageUrl: string) => {
+      if (loadedImageUrl === imageUrl) {
+        this.appFacade.updateSelectedImage(imageUrl, imageId, horizontal);
+        this.appFacade.openModal();
+      }
+    });
+  }
+
+  // Puts the Image Url inside an array which then can be used to check if the image has been loaded or not.
+  // If the image has not been loaded inside the openModal method we will map through the array and handle it accordingly based off if the
+  // imageUrl is found or not.
+  public allowOpenModal(url: string): void {
+    this.loadedImagesUrl.push(url);
   }
 
 }
