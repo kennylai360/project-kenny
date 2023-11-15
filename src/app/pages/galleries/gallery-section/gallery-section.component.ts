d3f1d4e99dbd443c1d9b85e10427808de5812167
Kenny Lai <kennylai360@gmail.com>
2023-11-15 19:48:56 +0000

    Updated gallery-section to be standalone component

--- src/app/pages/galleries/gallery-section/gallery-section.component.ts
+++ src/app/pages/galleries/gallery-section/gallery-section.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { RouterModule } from '@angular/router'
 
 @Component({
   selector: 'app-gallery-section',
   templateUrl: './gallery-section.component.html',
   styles: [],
+  standalone: true,
+  imports: [RouterModule],
 })
-export class GallerySectionComponent {
-}
+export class GallerySectionComponent {}
