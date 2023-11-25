b1802048bf989a1c3dccd4a7de9940c754e3e66a
Kenny Lai <kennylai360@gmail.com>
2023-11-25 15:15:13 +0000

    updated some css and some namings of css

--- src/app/pages/galleries/gallery-album/gallery-album.component.ts
+++ src/app/pages/galleries/gallery-album/gallery-album.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
-import { Observable, of, Subscription } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
-import { GalleryFacade } from '../../../state-management/gallery-list/gallery.facade';
-import { IGalleryCover } from '../../../state-management/gallery-list/gallery-cover.interface';
-import { AppFacade } from '../../../state-management/app/app.facade';
+import { Component, OnDestroy, OnInit } from '@angular/core'
+import { ActivatedRoute, Params } from '@angular/router'
+import { Observable, of, Subscription } from 'rxjs'
+import { debounceTime } from 'rxjs/operators'
+import { GalleryFacade } from '../../../state-management/gallery-list/gallery.facade'
+import { IGalleryCover } from '../../../state-management/gallery-list/gallery-cover.interface'
+import { AppFacade } from '../../../state-management/app/app.facade'
 
 @Component({
   selector: 'app-gallery-album',
@@ -12,55 +12,55 @@ import { AppFacade } from '../../../state-management/app/app.facade';
   styleUrls: ['./gallery-album.component.scss'],
 })
 export class GalleryAlbumComponent implements OnInit, OnDestroy {
+  public pageNumber: number = 1
 
-  public p: number = 1;
+  public selectedImageId: number = null
 
-  public selectedImageId: number = null;
+  private routeSubscription: Subscription
 
-  private routeSubscription: Subscription;
-
-  private loadedImagesUrl: Array<string> = [];
+  private loadedImagesUrl: Array<string> = []
 
   public albumData: Observable<IGalleryCover> = of({
     albumId: '',
-    imgUrl: ''
-  });
+    imgUrl: '',
+  })
 
-  constructor(private activatedRoute: ActivatedRoute,
-              private galleryFacade: GalleryFacade,
-              private appFacade: AppFacade
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private galleryFacade: GalleryFacade,
+    private appFacade: AppFacade
   ) {}
 
   ngOnInit() {
     // load the album here which corresponds to the id. If it does not exist then redirect to album listing page
     // Debouncetime is added to give the loading and make sure the gallery data has been loaded into the store.
     // this is as a fallback in case the use enters the ID in directly to the address bar.
-    this.routeSubscription = this.activatedRoute.params.pipe(debounceTime(10))
-    .subscribe((params: Params) => {
-      this.galleryFacade.setSelectedId(params['id']);
-      this.galleryFacade.getAlbumDataById();
-      this.albumData = this.galleryFacade.albumData$;
-    });
+    this.routeSubscription = this.activatedRoute.params
+      .pipe(debounceTime(10))
+      .subscribe((params: Params) => {
+        this.galleryFacade.setSelectedId(params['id'])
+        this.galleryFacade.getAlbumDataById()
+        this.albumData = this.galleryFacade.albumData$
+      })
   }
 
   ngOnDestroy() {
-    this.routeSubscription.unsubscribe();
+    this.routeSubscription.unsubscribe()
   }
 
   public openModal(imageId: number, imageUrl: string, horizontal: boolean) {
     this.loadedImagesUrl.map((loadedImageUrl: string) => {
       if (loadedImageUrl === imageUrl) {
-        this.appFacade.updateSelectedImage(imageUrl, imageId, horizontal);
-        this.appFacade.openModal();
+        this.appFacade.updateSelectedImage(imageUrl, imageId, horizontal)
+        this.appFacade.openModal()
       }
-    });
+    })
   }
 
   // Puts the Image Url inside an array which then can be used to check if the image has been loaded or not.
   // If the image has not been loaded inside the openModal method we will map through the array and handle it accordingly based off if the
   // imageUrl is found or not.
   public allowOpenModal(url: string): void {
-    this.loadedImagesUrl.push(url);
+    this.loadedImagesUrl.push(url)
   }
-
 }
