1bbbfe14461ff1434b17a9bf6f0969f94129b722
Kenny Lai <kennylai360@gmail.com>
2023-11-25 16:21:55 +0000

    Removed some unused code and cleaned up the css and renamed it a bit more consistent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,12 @@
-import { Component } from '@angular/core';
-import { GalleryFacade } from './state-management/gallery-list/gallery.facade';
+import { Component } from '@angular/core'
+import { GalleryFacade } from './state-management/gallery-list/gallery.facade'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   constructor(private galleryFacade: GalleryFacade) {
-    this.galleryFacade.loadGalleryList();
+    this.galleryFacade.loadGalleryList()
   }
 }
-
-
