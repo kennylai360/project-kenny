68810fa481c4c9f69018946233f64300662de316
Kenny Lai <kennylai360@gmail.com>
2023-12-04 22:46:03 +0000

    Fixed a small bug with the jumbotron logo not being cosnsitent and also fixed footer not being at the right place on different screen sizes

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,15 @@
-import { Component } from '@angular/core';
-import { environment } from '../../../environments/environment';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core'
+import { environment } from '../../../environments/environment'
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
   standalone: true,
-  imports: [CommonModule]
+  imports: [CommonModule],
 })
 export class FooterComponent {
-  public currentYear: number = new Date().getFullYear();
-  public version: string = environment.appVersion;
+  public currentYear: number = new Date().getFullYear()
+  public version: string = environment.appVersion
 }
