65c88511e29093d49ad9a835593abc587a17a128
Kenny Lai <kennylai360@gmail.com>
2024-01-04 21:09:51 +0000

    Updated the Not Found page

--- src/app/pages/notFound/notFound.component.ts
+++ src/app/pages/notFound/notFound.component.ts
@@ -1,12 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './notFound.component.html',
   styleUrls: ['./notFound.component.scss'],
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule],
 })
-export class NotFoundComponent {}
+export class NotFoundComponent {
+  public requestedUrl: string;
+
+  public constructor(private router: Router, private titleService: Title) {
+    this.requestedUrl = this.router.url;
+    this.titleService.setTitle('Error 404 - Page not found!');
+  }
+}
