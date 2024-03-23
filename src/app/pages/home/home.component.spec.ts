90157ffe50eaee67f9b0c11364224ffacd90f9ba
Kenny Lai <kennylai360@gmail.com>
2024-03-23 19:38:56 +0000

    Deleted unused tests

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,6 +1,5 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { By } from '@angular/platform-browser';
 import { HomeComponent } from './home.component';
 
 describe('Home component', () => {
@@ -14,35 +13,35 @@ describe('Home component', () => {
     fixture = TestBed.createComponent(HomeComponent);
   });
 
-  describe('Links and attributes', () => {
-    it('should test the resume link will have routerLink to /resume', () => {
-      const resumeRouterLink = fixture.debugElement
-        .query(By.css('a[data-qa="qa-resume-link"]'))
-        .nativeElement.getAttribute('routerLink');
-      expect(resumeRouterLink).toEqual('/resume');
-    });
+  // describe('Links and attributes', () => {
+  //   it('should test the resume link will have routerLink to /resume', () => {
+  //     const resumeRouterLink = fixture.debugElement
+  //       .query(By.css('a[data-qa="qa-resume-link"]'))
+  //       .nativeElement.getAttribute('routerLink');
+  //     expect(resumeRouterLink).toEqual('/resume');
+  //   });
 
-    it('should test the photography link will have routerLink to /photography', () => {
-      const photographyRouterLink = fixture.debugElement
-        .query(By.css('a[data-qa="qa-photography-link"]'))
-        .nativeElement.getAttribute('routerLink');
-      expect(photographyRouterLink).toEqual('/photography');
-    });
+  //   it('should test the photography link will have routerLink to /photography', () => {
+  //     const photographyRouterLink = fixture.debugElement
+  //       .query(By.css('a[data-qa="qa-photography-link"]'))
+  //       .nativeElement.getAttribute('routerLink');
+  //     expect(photographyRouterLink).toEqual('/photography');
+  //   });
 
-    it('should test the github link will have routerLink to the correct github url', () => {
-      const githubLink = fixture.debugElement
-        .query(By.css('a[data-qa="qa-github-link"]'))
-        .nativeElement.getAttribute('href');
-      expect(githubLink).toEqual('https://www.github.com/kennylai360');
-    });
+  //   it('should test the github link will have routerLink to the correct github url', () => {
+  //     const githubLink = fixture.debugElement
+  //       .query(By.css('a[data-qa="qa-github-link"]'))
+  //       .nativeElement.getAttribute('href');
+  //     expect(githubLink).toEqual('https://www.github.com/kennylai360');
+  //   });
 
-    it('should check that the github link has a rel attribute with values of noreferrer noopener', () => {
-      const githubRelAttribute = fixture.debugElement
-        .query(By.css('a[data-qa="qa-github-link"]'))
-        .nativeElement.getAttribute('rel');
-      expect(githubRelAttribute).toEqual('noreferrer noopener');
-    });
-  });
+  //   it('should check that the github link has a rel attribute with values of noreferrer noopener', () => {
+  //     const githubRelAttribute = fixture.debugElement
+  //       .query(By.css('a[data-qa="qa-github-link"]'))
+  //       .nativeElement.getAttribute('rel');
+  //     expect(githubRelAttribute).toEqual('noreferrer noopener');
+  //   });
+  // });
 
   it('should check the home UI should match snapshots', () => {
     expect(fixture.nativeElement).toMatchSnapshot();
