8dadbc48993e3b6cd4127ce9189f5d508682b41a
Kenny Lai <kennylai360@gmail.com>
2025-06-19 21:20:57 +0100

    formatting

--- src/app/api/coincap.service.ts
+++ src/app/api/coincap.service.ts
@@ -3,7 +3,6 @@ import { Injectable, inject } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
 
-
 @Injectable({
   providedIn: 'root',
 })
@@ -11,14 +10,10 @@ export class CoincapService {
   private httpClient: HttpClient = inject(HttpClient);
 
   public getBtcAssetData(): Observable<any> {
-    return this.httpClient.get(
-      environment.coinCapApi + `/bitcoin-markets`
-    );
+    return this.httpClient.get(environment.coinCapApi + `/bitcoin-markets`);
   }
 
   public getRatesData(): Observable<any> {
-    return this.httpClient.get(
-      environment.coinCapApi + `/rates`
-    );
+    return this.httpClient.get(environment.coinCapApi + `/rates`);
   }
 }
